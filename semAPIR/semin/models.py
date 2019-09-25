b5de78ed70c3e32f271a4268dc31425b061c6335
Luis Angel Pineda Urakami <yisangel@gmail.com>
2019-09-25 15:39:43 +0000

    backend model listo

--- semAPIR/semin/models.py
+++ semAPIR/semin/models.py
@@ -37,6 +37,13 @@ STATUS_CHOICES = (
     ('Des_inactiva','Desactivada_Inactiva'),
 )
 
+APPOINTMENT_STATUS_CHOICES = (
+('ACTIVA','ACTIVA'),
+('CERRADA','CERRADA'),
+('CANCELADA','CANCELADA'),
+('ESPERA','ESPERA'),
+)
+
 TITLE_CHOICES = (
     ('Dr.', 'Dr.'),
     ('Dra.','Dra.'),
@@ -77,6 +84,16 @@ STATE_CHOICES = (
     ('Zacatecas','Zacatecas'),
 )
 
+DAY_CHOICES = (
+    ('1', 'Lun'),
+    ('2', 'Mar'),
+    ('3', 'Mie'),
+    ('4', 'Jue'),
+    ('5', 'Vie'),
+    ('6', 'Sab'),
+    ('7', 'Dom'),
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     id_sem = models.IntegerField(null=True, blank=True)
@@ -91,6 +108,26 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
  
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=90,null=True, blank=False)
+    puesto = models.CharField(max_length=90,null=True, blank=False)
+    apellido_paterno = models.CharField(max_length=45,null=True, blank=False)
+    apellido_materno = models.CharField(max_length=45,null=True, blank=True)
+    sexo = models.CharField(max_length=9,null=True, blank=True, choices=SEX_CHOICES)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    telefono = models.IntegerField(null=True, blank=True)
+    email = models.EmailField(max_length=45, unique=True ,null=True, blank=True)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+
+    def __str__(self):
+        return '%s %s %s' % (self.nombre, self.apellido_paterno, self.apellido_materno)
+
+    class Meta:
+        verbose_name_plural = "Semin - Empleados"
+
+
 class Paciente(models.Model):
     nombre = models.CharField(max_length=90,null=True, blank=False)
     apellido_paterno = models.CharField(max_length=45,null=True, blank=False)
@@ -114,15 +151,15 @@ class Paciente(models.Model):
     num_exterior = models.CharField(max_length=10,null=True, blank=True)
     cp = models.CharField(max_length=5,null=True, blank=True)
     municipio = models.CharField(max_length=45,null=True, blank=True)
-    localidad = models.CharField(max_length=90,null=True, blank=True)
+    localidad = models.CharField(max_length=200,null=True, blank=True)
     estado = models.CharField(max_length=20,null=True, blank=True, choices=STATE_CHOICES)
     telefonoCasa = models.IntegerField(null=True, blank=True)
     telefonoOficina = models.IntegerField(null=True, blank=True)
     telefonoCelular = models.IntegerField(null=True, blank=True) 
     email = models.EmailField(max_length=45, unique=True ,null=True, blank=True)
     fecha_registro = models.DateField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True) # When it was create
-    updated_at = models.DateTimeField(auto_now=True) # When i was update
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
     creator = models.ForeignKey('auth.User', related_name='semin', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -130,4 +167,158 @@ class Paciente(models.Model):
 
     class Meta:
         verbose_name_plural = "Pacientes"
+
+
+class Expediente(models.Model):
+    numExpediente = models.CharField(max_length=90,null=True, blank=True)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+    Paciente = models.OneToOneField(Paciente, null=True, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.numExpediente, self.Paciente.nombre, self.Paciente.apellido_paterno, self.Paciente.apellido_materno)
+
+    class Meta:
+        verbose_name_plural = "Pacientes - Expedientes"
+
+
+class Estudios(models.Model):
+    fechaEstudio = models.DateField(null=True, blank=True)
+    tipoEstudio = models.CharField(max_length=90, null=True, blank=True)
+    urlEstudio = models.CharField(max_length=200, null=True, blank=True)
+    notasEstudio = models.CharField(max_length=500, null=True, blank=True)
+    Expediente = models.ForeignKey(Expediente, null=True, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s %s %s %s %s' % (self.fechaEstudio, self.tipoEstudio, self.Expediente.Paciente.nombre, self.Expediente.Paciente.apellido_paterno, self.Expediente.Paciente.apellido_materno)
+
+    class Meta:
+        verbose_name_plural = "Pacientes - Estudios"
+
+
+class Sucursal(models.Model):
+    nombreSucursal = models.CharField(max_length=90,null=True, blank=True)
+    descripcion = models.CharField(max_length=200,null=True, blank=True)
+    calle = models.CharField(max_length=90, null=True, blank=True)
+    colonia = models.CharField(max_length=45,null=True, blank=True)
+    num_interior = models.CharField(max_length=10,null=True, blank=True)
+    num_exterior = models.CharField(max_length=10,null=True, blank=True)
+    cp = models.CharField(max_length=5,null=True, blank=True)
+    municipio = models.CharField(max_length=45,null=True, blank=True)
+    localidad = models.CharField(max_length=200,null=True, blank=True)
+    estado = models.CharField(max_length=20,null=True, blank=True, choices=STATE_CHOICES)
+    telefono = models.IntegerField(null=True, blank=True)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+
+    def __str__(self):
+        return '%s' % (self.nombreSucursal)
+
+    class Meta:
+        verbose_name_plural = "Semin - Sucursales"
+
+
+class DisponibilidadServicios(models.Model):
+    tomaMuestras = models.BooleanField(default=False,null=True, blank=True)
+    ultrasonografia = models.BooleanField(default=False,null=True, blank=True)
+    rayosX = models.BooleanField(default=False,null=True, blank=True)
+    rayosXContrastados = models.BooleanField(default=False,null=True, blank=True)
+    mastografia = models.BooleanField(default=False,null=True, blank=True)
+    patologia = models.BooleanField(default=False,null=True, blank=True)
+    electrocardiograma = models.BooleanField(default=False,null=True, blank=True)
+    tomografia = models.BooleanField(default=False,null=True, blank=True)
+    resonanciaMagnetica = models.BooleanField(default=False,null=True, blank=True)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+    Sucursal = models.OneToOneField(Sucursal, null=True, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s, %s, %s, %s, %s, %s, %s, %s, %s,' % (self.tomaMuestras, self.ultrasonografia, self.rayosX, self.rayosXContrastados, self.mastografia, self.patologia, self.electrocardiograma, self.tomografia, self.resonanciaMagnetica)
+
+    class Meta:
+        verbose_name_plural = "Semin - Sucursales - Disponibilidad de Servicios"
+
+
+
+class HorarioSucursal(models.Model):
+    dia = models.CharField(max_length=1,null=True, blank=False, choices=DAY_CHOICES)
+    hora_inicio = models.TimeField(null=True, blank=False)
+    hora_final = models.TimeField(null=True, blank=False)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+    Sucursal = models.ForeignKey(Sucursal, null=True, blank=False, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return '%s, %s ,%s ,%s' % (self.dia, self.hora_inicio, self.hora_final, self.Sucursal.nombreSucursal)
+
+    class Meta:
+        verbose_name_plural = "Semin - Sucursales"
+
+
+class Cita(models.Model):
+    fecha_cita = models.DateField(null=True, blank=False)
+    titulo = models.CharField(max_length=45,null=True, blank=False)
+    descripcion = models.CharField(max_length=1000,null=True, blank=True)
+    notas = models.CharField(max_length=200,null=True, blank=False)
+    estatus = models.CharField(max_length=13,null=True, blank=True, choices=APPOINTMENT_STATUS_CHOICES)
+    hora_inicio = models.TimeField(null=True, blank=False)
+    hora_final = models.TimeField(null=True, blank=False)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+    Sucursal = models.ForeignKey(Sucursal, null=True, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s, %s' % (self.fecha_cita, self.Sucursal.nombreSucursal)
+
+    class Meta:
+        verbose_name_plural = "Semin - Sucursales - Citas"
+
+
+class Catalogo(models.Model):
+    area = models.CharField(max_length=200,null=True, blank=False)
+    prueba = models.CharField(max_length=200,null=True, blank=False)
+    sinAcro = models.CharField(max_length=200,null=True, blank=False)
+    beneficio = models.CharField(max_length=200,null=True, blank=False)
+    tipoMuestra = models.CharField(max_length=100,null=True, blank=False)
+    indicacionesPreExamen = models.CharField(max_length=1000,null=True, blank=False)
+    tiempoEntrega = models.CharField(max_length=45,null=True, blank=False)
+    precioVenta = models.FloatField(null=True, blank=False)
+    DIFPue = models.FloatField(null=True, blank=False)
+    ISSSTEPAtlixco = models.FloatField(null=True, blank=False)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+    Cita = models.ManyToManyField(Cita, blank=True)
+
+    def __str__(self):
+        return '%s, %s, %s, %s, %s' % (self.area, self.Sucursal.prueba, self.precioVenta, self.DIFPue, self.ISSSTEPAtlixco)
+
+    class Meta:
+        verbose_name_plural = "Semin - Catálogo de Estudios"
+
+
+class Promocion(models.Model):
+    titulo = models.CharField(max_length=200,null=True, blank=False)
+    descripcion = models.CharField(max_length=1000,null=True, blank=False)
+    precioVenta = models.FloatField(null=True, blank=False)
+    creacion = models.DateTimeField(auto_now_add=True) # When it was create
+    ultimaActualizacion = models.DateTimeField(auto_now=True) # When i was update
+    Catalogo = models.ManyToManyField(Catalogo, blank=True)
+
+    def __str__(self):
+        return '%s, %s' % (self.titulo, self.precioVenta)
+
+    class Meta:
+        verbose_name_plural = "Semin - Catálogo de Estudios - Promociones"
+
+
+
+
+
+
+
+
+
+
+
 
